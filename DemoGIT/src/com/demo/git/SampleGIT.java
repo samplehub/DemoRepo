4c55dd01974d57a7896d08818c46e3d0eeaa8178
sivadeepti <sivadeepti@BL-LAPTOP-12>
2016-06-20 11:01:18 +0100

    One more variable added

--- DemoGIT/src/com/demo/git/SampleGIT.java
+++ DemoGIT/src/com/demo/git/SampleGIT.java
@@ -5,7 +5,7 @@ public class SampleGIT {
 	public static void main(String[] args)
 	{
 		int i=0; //Initialized a  local variable
-		
+		int j=1;//one more
 		System.out.println("Welcome to GIT world");
 	}
 
